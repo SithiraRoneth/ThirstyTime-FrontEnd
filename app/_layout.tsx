a527cf2129da1967e46bd454132520e5e7624a70
USER <sithiraroneth@gmail.com>
2025-03-23 11:23:10 +0530

    edit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,37 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
-
-// import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
+import SplashScreen from '../app/SplashScreen';
+import LoginScreen from '../app/LoginScreen';
+import SignUpScreen from '../app/SignUpScreen';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import TabLayout from '../app/(tabs)/_layout'
+import ViewAllDetails from '../app/ViewAllDetails'
+import ViewDetails from '../app/ViewDetails'
+const Stack = createNativeStackNavigator();
 export default function RootLayout() {
-    // const colorScheme = useColorScheme();
     const [loaded] = useFonts({
-        // SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     });
-
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync();
-        }
-    }, [loaded]);
 
     if (!loaded) {
         return null;
     }
 
     return (
-    <>
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                {/* Auth Flow */}
+                <Stack.Screen name="Splash" component={SplashScreen}  options={{ headerShown: false }}/>
+                <Stack.Screen name="Login" component={LoginScreen}  options={{ headerShown: false }}/>
+                <Stack.Screen name="SignUp" component={SignUpScreen}  options={{ headerShown: false }}/>
 
-            </Stack>
-        <StatusBar style="auto" />
-    </>
+                {/* Main App Flow */}
+                <Stack.Screen name="Main" component={TabLayout} options={{ headerShown: false }} />
+                <Stack.Screen name="ViewAllDetails" component={ViewAllDetails} options={{ headerShown: false }} />
+                <Stack.Screen name="ViewDetails" component={ViewDetails} options={{ headerShown: false }} />
+            </Stack.Navigator>
+            <StatusBar />
+        </>
     );
 }
